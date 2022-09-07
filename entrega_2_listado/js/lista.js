00d061368dbfea79977b1fca61234d94c3a58112
rivas.will <rivas.will.1000@gmail.com>
2022-09-07 16:01:30 -0300

    updates xD

--- entrega_2_listado/js/lista.js
+++ entrega_2_listado/js/lista.js
@@ -19,6 +19,9 @@ insertarLS =()=>{
 
     localStorage.setItem('titulo',l_t);
     localStorage.setItem('descripcion',l_d);
+
+    descripcion.value='';
+    titulo.value ='';
 }
 
 imprimirLS =()=>{
@@ -27,7 +30,7 @@ imprimirLS =()=>{
     let contenido = '';
     l_t.forEach((e, i) => {
         contenido += `
-        <div class="card cnt tareas">
+        <div class="card cnt tareas${i}">
         <div class="card-body">
             <div class="row justify-content-center">
                 <div class="col-7">
@@ -36,10 +39,10 @@ imprimirLS =()=>{
                 </div>
                 <div class="col-1">
                     <ul class="list-group col-sm-0">
-                      <li class="list-group-item btn editar${i}"><i class="bi bi-pencil-square"></i></li>
-                      <li class="list-group-item btn completar${i}"><i class="bi bi-check-square"></i></li>
-                      <li class="list-group-item btn importante${i}"><i class="bi bi-exclamation-square"></i></li>
-                      <li class="list-group-item btn eliminar${i}"><i class="bi bi-x-square"></i></li>
+                      <li class="list-group-item btn editar" onclick="editar(${i})"><i class="bi bi-pencil-square"></i></li>
+                      <li class="list-group-item btn completar" onclick="cambiarColor('completar',${i})"><i class="bi bi-check-square"></i></li>
+                      <li class="list-group-item btn importante" onclick="cambiarColor('importante',${i})"><i class="bi bi-exclamation-square"></i></li>
+                      <li class="list-group-item btn eliminar" onclick="eliminar(${i})"><i class="bi bi-x-square"></i></li>
                     </ul>
                 </div>
             </div>
@@ -52,36 +55,32 @@ imprimirLS =()=>{
     cnt.innerHTML = contenido;
 }
 
-// acá agrego los adeventlistener a todos los botones
-const escuchadores =(a)=>{
-    a.forEach((e,i) => {
-        e.addEventListener('click', ()=>{
-            console.log('hice click en boton ' + i)
-        })
-    });
-}
 
 //aca quiero hacer la funcion que cambie de color
-const cambiarColor =()=>{
+const cambiarColor =(color,i)=>{
+    if(color == 'completar'){
+        document.querySelector(`.tareas${i}`).style.backgroundColor = 'lightgreen'
+    }else if(color == 'importante'){
+        document.querySelector(`.tareas${i}`).style.backgroundColor = 'lightyellow'
+    }
 
+}
+//eliminar
+const eliminar =(i)=>{
+    listaTitulos = listaTitulos.filter((elem,ind)=>ind!=i);
+    listaDescript = listaDescript.filter((elem,ind)=>ind!=i);
+    
+    let l_t = JSON.stringify(listaTitulos);
+    let l_d = JSON.stringify(listaDescript);
+    
+    localStorage.setItem('titulo',l_t);
+    localStorage.setItem('descripcion',l_d);
+    
+    imprimirLS();
 }
 
 agg.addEventListener('click', ()=>{
     insertarLS();
     imprimirLS();
-
-
-    //con esto selecciono todos los botones y les agrego los addeventListener
-    botonesEditar = document.querySelectorAll('[class *= "editar"]');
-    escuchadores(botonesEditar);
-
-    botonesCompletar = document.querySelectorAll('[class *= "completar"]');
-    escuchadores(botonesCompletar);
-
-    botonesImportante = document.querySelectorAll('[class *= "importante"]');
-    escuchadores(botonesImportante);
-
-    botonesEliminar = document.querySelectorAll('[class *= "eliminar"]');
-    escuchadores(botonesEliminar);
 })
 
