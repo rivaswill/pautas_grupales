4eaec0751022999fe2065e827dc8b60d337fa863
rivas.will <rivas.will.1000@gmail.com>
2022-10-01 00:27:43 -0300

    update

--- JAPFLIX/js/main.js
+++ JAPFLIX/js/main.js
@@ -16,11 +16,33 @@ const search =()=>{
 
 const getHTML =(e)=>{
     let get = `
-        <li class="list-group-item mt-3">
-          <h2>${e.title}</h2>
-          <p>${e.tagline}</p>
-          <span>${star(e)}</span>
-        </li>
+    <li class="list-group-item mt-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+        <h2>${e.title}</h2>
+        <p>${e.tagline}</p>
+        <span>${star(e)}</span>
+    </li>
+
+    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasTopLabel">${e.title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <p>${e.overview}</p>
+            <p>${e.genres.map(f=>f.name)}</p>
+        </div>
+        <div class="btn-group">
+            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              more...
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><button class="dropdown-item">Year: ${e.release_date.substring(0,4)}</button></li>
+              <li><button class="dropdown-item">Runtime: ${e.runtime} mins</button></li>
+              <li><button class="dropdown-item">Budget: $${e.budget}</button></li>
+              <li><button class="dropdown-item">Revenues: $${e.revenue}</button></li>
+            </ul>
+          </div>
+    </div>
     `
     return get;
 }
@@ -31,7 +53,7 @@ const star=(e)=>{
     let star =``;
 
     for(let i = 1; i<=numStar;i++){
-        star += `<i class="fa fa-star checked"></i>`
+        star += `<i class="bi bi-star-fill"></i>`
     }
     if(mod!=0){star += `<i class="bi bi-star-half"></i>`}
     return star;
@@ -48,7 +70,6 @@ const insertDATA=(e)=>{
 document.addEventListener('DOMContentLoaded', async()=>{
     const pelis = await getData(MOVIES_JAP);
     console.log(pelis);
-    //titulo genero tagline overview
 
     btn.addEventListener('click',()=>{
 
@@ -61,5 +82,5 @@ document.addEventListener('DOMContentLoaded', async()=>{
 
         insertDATA(busPelis);
     })
-    //titulo tagline votos
+    
 })
